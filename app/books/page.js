58bbf80a981c37fc72b554818fdfbde4b9651105
Clyde <ccrbalaman@mcm.edu.ph>
2026-01-08 14:15:52 +0800

    added a basic SEO techniques

--- app/books/page.js
+++ app/books/page.js
@@ -7,7 +7,7 @@ export default function BooksPage() {
 
       <div className="grid md:grid-cols-3 gap-6">
         <div className="bg-white p-4 rounded shadow">
-          <Image src="/images/book1.png" alt="Book" width={300} height={400} />
+          <Image src="/images/book1.png" alt="This is a book of a Modern CSS With Tailwind" width={300} height={400} />
           <h3 className="mt-3 font-medium">Modern CSS with Tailwind</h3>
         </div>
 
